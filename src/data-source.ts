ffda0310864784a75f214d03a3bfd5ca33723603
helenarodmun <helenarodmun@gmail.com>
2023-06-28 20:34:04 +0100

    Inicializado proyecto, creadas dos entidades, insertados registros en la bd

---
+++ src/data-source.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entity/User";
+import { Photo } from "./entity/Photo";
+
+export const AppDataSource = new DataSource({
+    type: "mariadb",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "Hekia*2020",
+    database: "testorm",
+    synchronize: true,//ensures that the entities will be synchronised with the database, each time the application is run.
+    logging: false,
+    entities: [User, Photo],
+    migrations: [],
+    subscribers: [],
+})
